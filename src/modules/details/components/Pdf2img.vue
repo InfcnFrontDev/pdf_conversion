c2de8164aa3a4004cbed8cb0f782d5f7320aeb7d
guoxm <guoxm@infcn.com.cn>
2016-09-13 09:48:25 +0800

    3个改

---
+++ src/modules/details/components/Pdf2img.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-content>
+        <h1 slot="title">
+            <span>PDF</span>文档转换为<span>图片</span>
+        </h1>
+        <h3>pdf to pic</h3>
+    </v-content>
+</template>
+<script>
+    import VContent from './VContent.vue'
+
+    export default{
+        components: {
+            VContent
+        },
+        ready() {
+            console.log('PdfToPic.vue')
+        }
+    }
+</script>
