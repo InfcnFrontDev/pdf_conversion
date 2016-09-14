c82d0a57ec20cfabc090e53b9955bc019d2471d4
yangkk <yangkk@infcn.com.cn>
2016-09-14 15:31:30 +0800

    add pdf2html/pdf2txt

--- src/modules/details/components/Pdf2xls.vue
+++ src/modules/details/components/Pdf2xls.vue
@@ -6,7 +6,6 @@
             </h1>
             <progress></progress>
             <flow url="/PDFApi/pdf2excel"></flow>
-            <set-page-num></set-page-num>
             <result></result>
             <download></download>
         </div>
@@ -17,13 +16,12 @@
 <script>
     import Progress from './parts/Progress.vue'
     import Flow from './parts/Flow.vue'
-    import SetPageNum from './parts/SetPageNum.vue'
     import Result from './parts/Result.vue'
     import Download from './parts/Download.vue'
 
     export default{
         components: {
-            Progress, Flow, SetPageNum, Result, Download
+            Progress, Flow, Result, Download
         }
     }
 </script>
