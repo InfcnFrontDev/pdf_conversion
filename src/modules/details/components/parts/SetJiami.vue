75b47c454ad7a67efb903cf5e6afb0b4ef504c95
yangkk <yangkk@infcn.com.cn>
2016-10-24 15:20:13 +0800

    修改bug

--- src/modules/details/components/parts/SetJiami.vue
+++ src/modules/details/components/parts/SetJiami.vue
@@ -1,8 +1,7 @@
 <template>
     <div class="file3" style="display:block">
         <h3 class="jiami-span">设置密码</h3>
-        <input class="inp-jiami" v-model="jiaMi"  type="text" v-if="shifou"/>
-        <input class="inp-jiami" v-model="jiaMi"  type="text" disabled="disabled" v-else/>
+        <input class="inp-jiami" v-model="jiaMi"  type="text"/>
         <span class="jiami-title" v-show="show">请设置密码！(数字或字母)</span>
         <div style="height:20px;"></div>
         <label><input type="checkbox" v-model="shifou"> <span style="font-size: 14px; color: #595656;"> 勾选后为全文档加密，需要输入密码后查看文档内容；(默认为页面加密，禁止编辑、复制、打印操作。)</span></label>
@@ -58,18 +57,12 @@
         },
         methods:{
             valid(){
-                if(this.shifou){
-                    if(/^[A-Za-z0-9]+$/.test(this.jiaMi)){
-                        this.show=false;
-                        this.isValidate(true);
-                    }else{
-                        this.show=true;
-                        this.isValidate(false);
-                    }
-                }else{
-                    this.jiaMi = '';
+                if(/^[A-Za-z0-9]+$/.test(this.jiaMi)){
                     this.show=false;
                     this.isValidate(true);
+                }else{
+                    this.show=true;
+                    this.isValidate(false);
                 }
             }
         }
